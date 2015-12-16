af5e5beb7da343a9021c05f8b46adb595aea0093
Richard Littauer <richard.littauer@gmail.com>
2015-12-16 00:18:02 -0500

    Keep subfolders in gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 
 gulp.task('default', () => {
-    return gulp.src('src/img/projects/*')
+    return gulp.src('src/img/**/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('assets/img/projects/'));
+        .pipe(gulp.dest('assets/img'));
 });
