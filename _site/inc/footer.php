685dfa46efd1021f1861231e3a4c5e0d4730a38a
Richard Littauer <richard.littauer@gmail.com>
2016-08-20 16:51:49 -0400

    Removed site from git

--- _site/inc/footer.php
+++
@@ -1,5 +0,0 @@
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/us/"></a></div>
-
-<?php include("analytics.php")?>
-</body>
-</html>
--- _site/inc/sound.php
+++
@@ -1,4 +0,0 @@
-<p id="audioplayer_1">Sound</p>  
-        <script type="text/javascript" >  
-        AudioPlayer.embed("audioplayer_1", {soundFile: "inc/readers1.mp3"});  
-        </script>
