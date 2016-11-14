741ddc696c4a4c71841cc3f8e2682d38d0314a47
Richard Littauer <richard.littauer@gmail.com>
2016-11-14 13:40:39 +0900

    Update script

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,19 +1,17 @@
-(function() {
-    var quotes = $(".quotes")
-    var quoteIndex = -1
-    var duration = 1000
+// For quotes on the main page
+var quotes = $(".quotes")
+var quoteIndex = -1
+var duration = 1000
 
-    function showNextQuote() {
-        ++quoteIndex;
-        quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(duration)
-            .delay(duration)
-            .fadeOut(duration, showNextQuote)
-    }
+function showNextQuote() {
+    ++quoteIndex;
+    quotes.eq(quoteIndex % quotes.length)
+        .fadeIn(duration)
+        .delay(duration)
+        .fadeOut(duration, showNextQuote)
+}
 
-    showNextQuote()
+showNextQuote()
 
-    // Year
-    $(".year").text(function() { return new Date().getFullYear()})
-})()
-
+// Update copyright year
+$(".year").text(function() { return new Date().getFullYear()})
