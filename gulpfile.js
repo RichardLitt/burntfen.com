49fe9ecd124c0f86cd64ecf60fa6b5aa5ec15afa
Richard Littauer <richard+github@burntfen.com>
2026-04-15 20:23:32 +1200

    Edit gulpfile using claude

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,9 @@ const uglify = require('gulp-uglify')
 const changed = require('gulp-changed')
 const imagemin = require('gulp-imagemin')
 const rename = require('gulp-rename')
-const pngcrush = require('imagemin-pngcrush')
 const imageResize = require('gulp-image-resize')
 
-var paths = {
+const paths = {
   js: ['src/js/**/*.js', '!src/js/**/*.min.js'],
   jsMin: ['src/js/**/*.min.js'],
   img: ['src/img/**/*.png', 'src/img/**/*.jpg'],
@@ -31,107 +30,57 @@ var paths = {
   clientImg: ['src/img/clients/*.png', 'src/img/clients/*.jpg']
 }
 
-gulp.task('js', function () {
-  gulp.src(paths.js)
+gulp.task('js-min', function () {
+  return gulp.src(paths.js)
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('assets/js'))
-  gulp.src(paths.jsMin)
+})
+
+gulp.task('js-copy', function () {
+  return gulp.src(paths.jsMin)
     .pipe(gulp.dest('assets/js'))
 })
+
+gulp.task('js', gulp.series('js-min', 'js-copy'))
+
+const imageminPlugins = [
+  imagemin.jpegtran({ progressive: true }),
+  imagemin.optipng({ optimizationLevel: 3 })
+]
+
+function resizeAndMinify (src, dest, width) {
+  return gulp.src(src)
+    .pipe(imageResize({ width, crop: false, upscale: false }))
+    .pipe(imagemin(imageminPlugins, { verbose: true }))
+    .pipe(gulp.dest(dest))
+}
 
 gulp.task('project-img', function () {
   return gulp.src(paths.projectImg)
     .pipe(changed('assets/img/project'))
-    .pipe(imageResize({
-      width: 250,
-      crop: false,
-      upscale: false
-    }))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngcrush()]},
-      {verbose: true}
-    ))
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3})
-    ],
-    {verbose: true}
-    ))
+    .pipe(imageResize({ width: 250, crop: false, upscale: false }))
+    .pipe(imagemin(imageminPlugins, { verbose: true }))
     .pipe(rename({ suffix: '-200' }))
     .pipe(gulp.dest('assets/img/project'))
 })
 
 gulp.task('press-img', function () {
-  return gulp.src(paths.pressImg)
-    // .pipe(changed('assets/img/press'))
-    .pipe(imageResize({
-      width: 150,
-      crop: false,
-      upscale: false
-    }))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngcrush()]
-    }))
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3})
-    ],
-    {verbose: true}
-    ))
-    // .pipe(rename({ suffix: '' }))
-    .pipe(gulp.dest('assets/img/press'))
+  return resizeAndMinify(paths.pressImg, 'assets/img/press', 150)
 })
 
 gulp.task('client-img', function () {
-  return gulp.src(paths.clientImg)
-    // .pipe(changed('assets/img/clients'))
-    .pipe(imageResize({
-      width: 150,
-      crop: false,
-      upscale: false
-    }))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngcrush()]
-    }))
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3})
-    ],
-    {verbose: true}
-    ))
-    // .pipe(rename({ suffix: '' }))
-    .pipe(gulp.dest('assets/img/clients'))
+  return resizeAndMinify(paths.clientImg, 'assets/img/clients', 150)
 })
 
 gulp.task('img', function () {
   return gulp.src(paths.img)
     .pipe(changed('assets/img'))
-    .pipe(imageResize({
-      width: 2000,
-      crop: false,
-      upscale: false
-    }))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngcrush()]
-    }))
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3})
-    ],
-    {verbose: true}
-    ))
+    .pipe(imageResize({ width: 2000, crop: false, upscale: false }))
+    .pipe(imagemin(imageminPlugins, { verbose: true }))
     .pipe(gulp.dest(paths.imgDest))
 })
 
-gulp.task('images', gulp.series('img', 'project-img', 'press-img'))
+gulp.task('images', gulp.series('img', 'project-img', 'press-img', 'client-img'))
 gulp.task('resize', gulp.series('project-img'))
-gulp.task('default', gulp.series('js', 'img', 'project-img', 'press-img'))
+gulp.task('default', gulp.series('js', 'img', 'project-img', 'press-img', 'client-img'))
